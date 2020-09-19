a05f048b943efaaaaac1253fe1f05fbc1de6552c
RystaC <mirage.s.cpp@gmail.com>
2020-09-20 05:28:55 +0900

    add image creation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use std::fs::File;
 use std::io::{Write, BufWriter};
 
-fn main() {
-    let mut file = BufWriter::new(File::create("products/test.txt").expect("failed to create file."));
+#[derive(Clone)]
+struct RGB {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
-    file.write_all("Hello everyone.\n".as_bytes()).unwrap();
-    file.write_all("This is test file.\n".as_bytes()).unwrap();
+fn main() {
+    let width: usize = 100;
+    let height: usize = 100;
+
+    let mut buffer: Vec<Vec<RGB>> = Vec::with_capacity(height);
+    buffer.resize(height, Vec::with_capacity(width));
+    for x in &mut buffer { x.resize(width, RGB{ red: 0, green: 255, blue: 0 }); }
+
+
+    let mut file = BufWriter::new(File::create("products/test.ppm").expect("failed to create file."));
+
+    file.write_all("P6\n".as_bytes()).unwrap();
+    file.write_all(format!("{} {}\n", width, height).as_bytes()).unwrap();
+    file.write_all("255\n".as_bytes()).unwrap();
+
+    for i in 0..height {
+        for j in 0..width {
+            file.write_all(&[buffer[i][j].red, buffer[i][j].green, buffer[i][j].blue]).unwrap();
+        }
+    }
+
 }
