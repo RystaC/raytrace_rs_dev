67eb32cf9ba658a3252e56e9fc8811188c69e91a
RystaC <mirage.s.cpp@gmail.com>
2020-09-20 04:47:49 +0900

    add file writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io::{Write, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = BufWriter::new(File::create("products/test.txt").expect("failed to create file."));
+
+    file.write_all("Hello everyone.\n".as_bytes()).unwrap();
+    file.write_all("This is test file.\n".as_bytes()).unwrap();
 }
