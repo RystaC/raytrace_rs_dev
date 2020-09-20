dbd4c0ae7b72425e17ade895db80862b4cd2d29b
RystaC <mirage.s.cpp@gmail.com>
2020-09-21 06:09:10 +0900

    add rng

---
+++ src/xorshift.rs
@@ -0,0 +1,31 @@
+pub struct XorShift {
+    x: u64,
+    y: u64,
+    z: u64,
+    w: u64,
+}
+
+impl XorShift {
+    pub fn new(seed: u64) -> Self {
+        let mut s = seed;
+        let mut entity = XorShift { x: 123456789, y: 362436069, z: 521288629, w: 88675123 };
+        s = (s ^ (s >> 30) + 0).wrapping_mul(1812433253);
+        entity.x = s;
+        s = (s ^ (s >> 30) + 1).wrapping_mul(1812433253);
+        entity.y = s;
+        s = (s ^ (s >> 30) + 2).wrapping_mul(1812433253);
+        entity.z = s;
+        s = (s ^ (s >> 30) + 3).wrapping_mul(1812433253);
+        entity.w = s;
+        entity
+    }
+
+    pub fn next(&mut self) -> u64 {
+        let t = self.x ^ (self.x << 11);
+        self.x = self.y;
+        self.y = self.z;
+        self.z = self.w;
+        self.w = (self.w ^ (self.w >> 19)) ^ (t ^ (t >> 8));
+        self.w
+    }
+}
--- tests/vector.rs
+++ tests/vector.rs
@@ -3,46 +3,42 @@ extern crate raytrace_rs;
 use raytrace_rs::vector::*;
 
 #[test]
-fn test_add() {
+fn test_vector_add() {
     let x = Vector3::new(3.0, 2.0, 1.0);
     let y = Vector3::new(7.0, 3.0, 1.0);
 
     assert_eq!(x + y, Vector3{ x: 10.0, y: 5.0, z: 2.0 });
-
-    let _z = x + y;
 } 
 
 #[test]
-fn test_sub() {
+fn test_vector_sub() {
     let x = Vector3::new(3.0, 2.0, 1.0);
     let y = Vector3::new(7.0, 3.0, 1.0);
 
     assert_eq!(x - y, Vector3{ x: -4.0, y: -1.0, z: 0.0 });
-
-    let _z = x + y;
 }
 
 #[test]
-fn test_mul_vf() {
+fn test_vector_mul_vf() {
     let x = Vector3::new(1.0, 2.0, 3.0);
     assert_eq!(x * 2.0, Vector3{ x: 2.0, y: 4.0, z: 6.0 });
 }
 
 #[test]
-fn test_mul_fv() {
+fn test_vector_mul_fv() {
     let x = Vector3::new(1.0, 2.0, 3.0);
     assert_eq!(2.0 * x, Vector3{ x: 2.0, y: 4.0, z: 6.0 });
 }
 
 #[test]
-fn test_dot() {
+fn test_vector_dot() {
     let x = Vector3::new(1.0, -2.0, 3.0);
     let y = Vector3::new(3.0, 2.0, -1.0);
     assert_eq!(dot(x, y), -4.0)
 }
 
 #[test]
-fn test_cross() {
+fn test_vector_cross() {
     let x = Vector3::new(1.0, 2.0, 3.0);
     let y = Vector3::new(3.0, 4.0, 5.0);
     assert_eq!(cross(x, y), Vector3{ x: -2.0, y: 4.0, z: -2.0});
