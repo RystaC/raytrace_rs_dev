5663698c8de8f32108be3d32b7cf311a2b03a08c
RystaC <mirage.s.cpp@gmail.com>
2020-09-27 02:59:01 +0900

    add gamma correction for image

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 extern crate raytrace_rs;
 
 use std::process;
-use std::time::SystemTime;
+//use std::time::SystemTime;
 
-use raytrace_rs::xorshift::XorShift;
+//use raytrace_rs::xorshift::XorShift;
 use raytrace_rs::ppm_gen::*;
 
 fn main() {
-    let width: usize = 100;
-    let height: usize = 100;
+    let width: usize = 256;
+    let height: usize = 256;
 
-    let mut rand = XorShift::new(SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("failed to init RNG.").as_nanos() as u64);
+    //let mut rand = XorShift::new(SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("failed to init RNG.").as_nanos() as u64);
 
     let mut buffer: Vec<Vec<RGB>> = Vec::with_capacity(height);
     buffer.resize(height, Vec::with_capacity(width));
-    for x in &mut buffer { x.resize(width, RGB::new(0, 0, 0)); }
+    for x in &mut buffer { x.resize(width, RGB::new(0.0, 0.0, 0.0)); }
 
     for i in 0..height {
         for j in 0..width {
-            buffer[i][j] = RGB::new((rand.next() % 256) as u8, (rand.next() % 256) as u8, (rand.next() % 256) as u8);
+            //buffer[i][j] = RGB::new((rand.next() % 256) as u8, (rand.next() % 256) as u8, (rand.next() % 256) as u8);
+            buffer[i][j] = RGB::new(i as f64 / (width - 1) as f64, j as f64 / (height - 1) as f64, 0.25);
         }
     }
 
     if let Err(error) = generate_ppm(&buffer) {
-        eprintln!("Error detected in generating ppm file.");
+        eprintln!("\nError detected in generating ppm file.");
         eprintln!("Original error: {}", error);
         process::exit(1);
     }
--- src/ppm_gen.rs
+++ src/ppm_gen.rs
@@ -4,30 +4,45 @@ use std::error::Error;
 
 #[derive(Clone, Copy, Debug)]
 pub struct RGB {
-    red: u8,
-    green: u8,
-    blue: u8,
+    red: f64,
+    green: f64,
+    blue: f64,
 }
 
 impl RGB {
-    pub fn new(red: u8, green: u8, blue: u8) -> Self {
-        RGB { red, green, blue }
+    pub fn new(red: f64, green: f64, blue: f64) -> Self {
+        RGB { red: clamp(red), green: clamp(green), blue: clamp(blue) }
     }
 
     pub fn as_bytes(&self) -> [u8; 3] {
-        [self.red, self.green, self.blue]
+        [gamma_correct(self.red), gamma_correct(self.green), gamma_correct(self.blue)]
     }
 }
 
+#[inline(always)]
+fn clamp(x: f64) -> f64 {
+    if x < 0.0 { 0.0 }
+    else if x > 1.0 { 1.0 }
+    else { x }
+}
+
+#[inline(always)]
+fn gamma_correct(x: f64) -> u8 {
+    (x.powf(1.0 / 2.2) * 255.0 + 0.5) as u8
+}
+
 pub fn generate_ppm(buffer: &Vec<Vec<RGB>>) -> Result<(), Box<dyn Error>> {
+    let width = buffer[0].len();
+    let height = buffer.len();
+
     let mut file = BufWriter::new(File::create("products/test.ppm")?);
 
     file.write_all("P6\n".as_bytes())?;
-    file.write_all(format!("{} {}\n", buffer[0].len(), buffer.len()).as_bytes())?;
+    file.write_all(format!("{} {}\n", width, height).as_bytes())?;
     file.write_all("255\n".as_bytes())?;
 
-    for i in 0..buffer.len() {
-        for j in 0..buffer[0].len() {
+    for i in 0..height {
+        for j in 0..width {
             file.write_all(&buffer[i][j].as_bytes())?;
         }
     }
